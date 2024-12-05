fe1865a027ea40a4fe0008613806c2012e3115bf
Mohammed N <mnaser11218@gmail.com>
2024-12-04 19:20:08 -0500

    created sections of website:

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,7 +1,31 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      Popular Products
+    </section>
+    <section className="padding">
+     super quality
+    </section>
+    <section className="padding-x py-10">
+      services
+    </section>
+    <section className="padding">
+    special offers
+    </section>
+    <section className="bg-pale-blue padding">
+   customer review 
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+    subscribe
+    </section> 
+     <section className="bg-black padding-x padding-t pb-8">
+    footer
+    </section>
+   </main>
   )
 }
