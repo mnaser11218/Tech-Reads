ce1264ac3e696b8e927c177b7c57f00781810b3d
Mohammed N <mnaser11218@gmail.com>
2024-12-05 16:35:43 -0500

    updated popular product card to show start and price

--- my-project/src/components/PopularProductCard.jsx
+++ my-project/src/components/PopularProductCard.jsx
@@ -1,3 +1,4 @@
+import { star } from "../assets/icons";
 
 function PopularProductCard({imgURL, name, price}) {
   return (
@@ -7,6 +8,19 @@ function PopularProductCard({imgURL, name, price}) {
       alt={name}
       className="w-[280px] h-[280px]"
       />
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img 
+        src={star}
+        alt="rating"
+        width={24}
+        height={24}
+        />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">(4.5)</p>
+       
+
+      </div>
+      <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
+      <p className="mt-2 font-semibold font-montserrat text-coral-red text-2lx leading-normal">{price}</p>
     </div>
   )
 }
