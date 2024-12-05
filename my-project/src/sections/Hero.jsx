a4df74f0aa8f93263d79d1dd42f489f6647d3d31
Mohammed N <mnaser11218@gmail.com>
2024-12-04 22:05:18 -0500

    added statistics in hero component

--- my-project/src/sections/Hero.jsx
+++ my-project/src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import { arrowRight } from "../assets/icons";
 import Button from "../components/Button";
+import { statistics } from "../constants";
 
 const  Hero = () => {
   return (
@@ -17,6 +18,20 @@ const  Hero = () => {
         </h1>
         <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
       <Button label="Shop now"  iconURL={arrowRight}/>
+
+      <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16">
+        {statistics.map((stat, index)=>(
+          <div>
+            <p>
+            {stat.value}
+            </p>
+            <p>
+            {stat.label}
+            </p>
+          </div>
+
+        ))}
+      </div>
       </div>
 
     </section>
