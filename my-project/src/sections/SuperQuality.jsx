948d0867fc63bec17095de89bcdef7e3539fc936
Mohammed N <mnaser11218@gmail.com>
2024-12-05 17:54:43 -0500

    added super quality section in page

--- my-project/src/sections/SuperQuality.jsx
+++ my-project/src/sections/SuperQuality.jsx
@@ -10,22 +10,24 @@ className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-c
   className="flex flex-1 flex-col"
   >
 
-    <h2>
-    <p className="text-xl font-montserrat text-coral-red">Our summer Collection</p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
-          <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span>
-          Shoes
-        </h1>
-        <p className="font-montserrat text-slate-gray text-lg leading- mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
-      <Button label="View details"  />
+  
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you <span className="text-coral-red">Super </span>
+          <span className="text-coral-red">Quality </span>
+         Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+     <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction
+</p>
+<div className="mt-11">
+<Button label="View details"  />
+</div>
+    
 
 
 
-    </h2>
+  
   </div>
-  testing
 </section>  )
 }
 
