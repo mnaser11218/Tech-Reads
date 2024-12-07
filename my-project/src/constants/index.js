be0e52551ac77b96a082f306220cb26897006aee
Mohammed N <mnaser11218@gmail.com>
2024-12-06 22:11:13 -0500

    completed review card component

--- my-project/src/constants/index.js
+++ my-project/src/constants/index.js
@@ -93,7 +93,7 @@ export const reviews = [
         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     },
     {
-        imgURL: customer2,
+        imgURL: customer1,
         customerName: 'Lota Mongeskar',
         rating: 4.5,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
