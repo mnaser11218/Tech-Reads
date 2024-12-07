94c989147a3c4d6fc6db6d3d30434b364dbcaa4b
Mohammed N <mnaser11218@gmail.com>
2024-12-06 21:38:23 -0500

    added review card component

--- my-project/src/sections/CustomerReviews.jsx
+++ my-project/src/sections/CustomerReviews.jsx
@@ -1,3 +1,5 @@
+import ReviewCard from "../components/ReviewCard"
+import { reviews } from "../constants"
 
 const CustomerReviews = () => {
   return (
@@ -9,7 +11,11 @@ const CustomerReviews = () => {
 
      <p className="info-text text-center m-auto mt-4 max-w-lg">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
 
-     <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14"></div>
+     <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+      {reviews.map(review=>(
+        <ReviewCard />
+      ))}
+     </div>
     </section>
   )
 }
